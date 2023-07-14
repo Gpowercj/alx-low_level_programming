65e519d5c367e735ac770cb2998efd40af18150f
Silas Godspower Chijioke <silasgchijioke@gmail.com>
2023-07-14 12:16:55 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int b;
+	b = 0;
+
+while(b <= 10)
+{
+printf("%d\t",b);
+++b;
+}
+putchar('\n');
+return (0);
+}
+
+
