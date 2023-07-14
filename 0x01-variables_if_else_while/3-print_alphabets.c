c3db6c2c94eab061b5dc28f750d6c57d2a7e35a9
Silas Godspower Chijioke <silasgchijioke@gmail.com>
2023-07-14 11:46:51 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ char alpha;
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 	putchar(alpha);
+	}
 		for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		{
 			putchar(alpha);
 		}
-	}
 	putchar('\n');
 	return (0);
 }
