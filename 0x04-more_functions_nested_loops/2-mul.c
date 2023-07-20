bfe70ca376c5b736ded238cd4361feb5ce77c83d
Silas Godspower Chijioke <silasgchijioke@gmail.com>
2023-07-20 09:22:11 -0700

    more nested loops and functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul(int a, int b) - multiplies integers a and b
+* @a: first int
+* @b: second int
+* return 0 - always success
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
